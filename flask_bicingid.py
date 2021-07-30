ac228e2cd48f0c1b00b9e5acd8948ede467d8ed7
Ferran <ferri.fc@gmail.com>
2021-07-30 18:44:53 +0200

    error id no encontrado solucinado

--- flask_bicingid.py
+++ flask_bicingid.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/id/<valor>')
 def profile(valor):
+	selected_station = "Id no encontrado"
 	r = requests.get('http://api.citybik.es/v2/networks/bicing')
 	bicingJson = r.json()
 	for station in bicingJson['network']['stations']:
